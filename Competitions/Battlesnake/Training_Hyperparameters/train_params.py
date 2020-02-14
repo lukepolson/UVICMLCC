cc88075d318d6f6348b357c23cf7d20aa138ce83
Luke Polson <lukepolson@outlook.com>
2020-02-14 09:03:14 -0800

    .

--- Competitions/Battlesnake/Training_Hyperparameters/train_params.py
+++ Competitions/Battlesnake/Training_Hyperparameters/train_params.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # This is a list of the trials that have been completed and data stored
-# Trial_0, Trial_1, Trial_2, Trial_3, Trial_4
+# Trial_0, Trial_1, Trial_2, Trial_3, Trial_4, Trial_5, Trial_6
 
 
 
@@ -110,7 +110,70 @@ trial_4 = {
 "n_iterations": 4000000
 }
 
-trials = [trial_0, trial_1, trial_2, trial_3, trial_4]
+# Same as trial_0, more NN params than trial_2
+trial_5 =  {
+"food_reward": 5,
+"step_reward": 1,
+"conv_layer_params": [[(16, (4, 4), 2), (32, (3 ,3), 1)]],
+"fc_layer_params": [[100, 100]],
+"optimizer_learning_rate": 2.5e-4,
+"optimizer_decay": 0.95,
+"optimizer_momentum": 0,
+"optimizer_epsilon": 0.00001,
+"epsilon_decay_steps": 100000,
+"epsilon_final": 0.01,
+"target_update_period": 2000,
+"discount_factor": 0.97,
+"init_replay_buffer": 20000,
+"dataset_sample_batch_size": 64,
+"dataset_num_steps": 2,
+"dataset_num_parallel_calls":3,
+"n_iterations": 4000000
+}
+
+# Same as trial_3 except more conv windows used
+trial_6 = {
+"food_reward": 5,
+"step_reward": 1,
+"conv_layer_params": [[(32, (3, 3), 1), (64, (2 ,2), 1)]],
+"fc_layer_params": [[50, 50]],
+"optimizer_learning_rate": 2.5e-4,
+"optimizer_decay": 0.95,
+"optimizer_momentum": 0,
+"optimizer_epsilon": 0.00001,
+"epsilon_decay_steps": 100000,
+"epsilon_final": 0.01,
+"target_update_period": 2000,
+"discount_factor": 0.97,
+"init_replay_buffer": 20000,
+"dataset_sample_batch_size": 64,
+"dataset_num_steps": 2,
+"dataset_num_parallel_calls":3,
+"n_iterations": 4000000
+}
+
+# Uses best things found so far: smaller conv windows, more NN params, smaller discount factor
+trial_7 = {
+"food_reward": 5,
+"step_reward": 1,
+"conv_layer_params": [[(16, (4, 4), 2), (32, (3 ,3), 1)]],
+"fc_layer_params": [[100, 100]],
+"optimizer_learning_rate": 2.5e-4,
+"optimizer_decay": 0.95,
+"optimizer_momentum": 0,
+"optimizer_epsilon": 0.00001,
+"epsilon_decay_steps": 100000,
+"epsilon_final": 0.01,
+"target_update_period": 2000,
+"discount_factor": 0.90,
+"init_replay_buffer": 20000,
+"dataset_sample_batch_size": 64,
+"dataset_num_steps": 2,
+"dataset_num_parallel_calls":3,
+"n_iterations": 4000000
+}
+
+trials = [trial_0, trial_1, trial_2, trial_3, trial_4, trial_5, trial_6, trial_7]
 dfs = [pd.DataFrame(trial) for trial in trials]
 df = pd.concat(dfs).reset_index()
 df.to_pickle('train_params.pkl')
